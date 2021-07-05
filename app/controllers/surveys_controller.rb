bedef3bc4822e11017d8e0196d0fdb142a7d5627
Ivan Christian Jay Funcion <icjfuncion@gmail.com>
2021-07-05 15:22:20 +0800

    fix session errors

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -11,7 +11,7 @@ class SurveysController < ApplicationController
 
     puts user_params[:name]    
     puts session[:current_name]
-    session[:n_time] = session[:n_time] + 1
+    session[:n_time] = (session[:n_time] ? session[:n_time] : 0) + 1
     session[:survey] =  user_params
     flash[:notice] = "Thanks for Submitting this form! You have sumitted this form #{session[:n_time]} times now"
     redirect_to "/surveys/result"
